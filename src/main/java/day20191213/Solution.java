24115a8bc7288d3f188418f02d95188155e60d2d
liyigang <18935117025@163.com>
2019-12-13 17:55:52 +0800

    update

--- src/main/java/day20191213/Solution.java
+++ src/main/java/day20191213/Solution.java
@@ -1,5 +1,7 @@
 package day20191213;
 
+import day20191211.ListNode;
+
 public class Solution {
     //读出N个数中第K个最大者
     public static int maxValInK(int[] arr,int k){
@@ -31,6 +33,54 @@ public class Solution {
         return resArr[k-1];
     }
 
+    // 输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+    public ListNode Merge(ListNode list1,ListNode list2) {
+        ListNode h1 = null;
+        ListNode p = null;
+        if(list1==null){
+            return list2;
+        }else if(list2==null){
+            return list1;
+        }
+        if(list1!=null&&list2!=null){
+            if(list1.val<=list2.val){
+                h1 = new ListNode(list1.val);
+                list1=list1.next;
+                p=h1;
+            }else{
+                h1 = new ListNode(list2.val);
+                list2 = list2.next;
+                p=h1;
+            }
+        }
+        while(list1!=null||list2!=null){
+            if(list1!=null&&list2!=null){
+                if(list1.val<=list2.val){
+                    h1.next=list1;
+                    h1=h1.next;
+                    list1=list1.next;
+                }else{
+                    h1.next=list2;
+                    h1=h1.next;
+                    list2 = list2.next;
+                }
+            }else if(list1==null&&list2!=null){
+                h1.next=list2;
+                h1=h1.next;
+                list2 = list2.next;
+            }else if(list1!=null&&list2==null){
+                h1.next=list1;
+                h1=h1.next;
+                list1=list1.next;
+            }
+        }
+        return p;
+    }
+
+    // TODO: 2019/12/13 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        return false;
+    }
     public static void main(String[] args) {
         int[] arr = {1,2,6,3,8,12,32,11,54,7,9};
         System.out.print(maxValInK(arr,7));
