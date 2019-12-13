b5410f077a26c5eac12fa537bf6521cdb1d366a2
liyigang <18935117025@163.com>
2019-12-13 18:07:12 +0800

    update

--- src/main/java/day20191213/Solution.java
+++ src/main/java/day20191213/Solution.java
@@ -79,6 +79,10 @@ public class Solution {
 
     // TODO: 2019/12/13 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
     public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        if(root1==null||root2==null){
+            return false;
+        }
+
         return false;
     }
     public static void main(String[] args) {
